f1a32649ec33c99367e612c387626542a66e7a99
nikolamirilo <nikolamirilo@gmail.com>
2023-06-19 00:46:04 +0200

    changed to ssr

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,5 +1,5 @@
 export async function getData(actionType:string) {
-  const res = await fetch(`http://localhost:3000/api/actions/${actionType}`, { cache: "no-store" });
+  const res = await fetch(`http://localhost:3000/api/actions/${actionType}`, { cache: "no-cache" });
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ module.exports = {
   images: {
     domains: ["th.bing.com", "png.pngtree.com", "images.unsplash.com", "tuk-cdn.s3.amazonaws.com"],
   },
-  experimental: { serverActions: true },
   env: {
     MONGODB_URI: "mongodb+srv://ekoloskimarketing:ekomarketing2023@cluster0.lnyhpwq.mongodb.net",
   },
