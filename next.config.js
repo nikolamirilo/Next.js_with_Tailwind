10d512237f09c44d97a025687f5ba26d2ea22623
Nikola <nikola.mirilo@adacta-fintech.com>
2023-06-19 21:59:18 +0200

    added rewrites

--- next.config.js
+++ next.config.js
@@ -6,4 +6,24 @@ module.exports = {
   env: {
     WEB_APP_URL: process.env.WEB_APP_URL,
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/actions/public",
+        destination: `${WEB_APP_URL}/*`,
+      },
+      {
+        source: "/api/actions",
+        destination: `${WEB_APP_URL}/admin*`,
+      },
+      {
+        source: "/api/actions/addNew",
+        destination: `${WEB_APP_URL}/doprinesi*`,
+      },
+      {
+        source: "/api/actions/update",
+        destination: `${WEB_APP_URL}/admin*`,
+      },
+    ];
+  },
 };
