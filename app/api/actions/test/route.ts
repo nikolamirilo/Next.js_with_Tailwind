fd016bdd131309ddf914382b093de5b030374ccc
nikolamirilo <nikolamirilo@gmail.com>
2023-06-21 00:32:04 +0200

    fix v4

--- app/api/actions/test/route.ts
+++ app/api/actions/test/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(request: NextRequest) {
   try {
    const random = Math.random();
-   return NextResponse.json(random)
+   return NextResponse.json(random, {headers:{"Cache-Control": "no-cache"}} )
   } catch (error:any) {
     return new Response(error.message);
   }
