6c73928c2413642fdaa76e7f2e271915f32aca61
nikolamirilo <nikolamirilo@gmail.com>
2023-06-21 00:02:23 +0200

    added check

--- components/Actions.tsx
+++ components/Actions.tsx
@@ -18,8 +18,10 @@ async function getData(actionType: string) {
 }
 
 const Actions: React.FC<ActionsProps> = async ({ title, actionType, type }) => {
+  if (!title || !actionType) {
+    return <div>Bla bla</div>;
+  }
   const data = await getData(actionType);
-  console.log(data);
   return (
     <div className="bg-white" id="akcije">
       <div className="mx-auto max-w-2xl sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 flex flex-col items-center justify-center gap-20">
