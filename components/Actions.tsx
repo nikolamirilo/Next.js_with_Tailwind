9a0b4db59b26d7af724e0fbab2429aebd22af6d7
nikolamirilo <nikolamirilo@gmail.com>
2023-06-21 00:15:52 +0200

    fix v2

--- components/Actions.tsx
+++ components/Actions.tsx
@@ -32,7 +32,7 @@ const Actions: React.FC<ActionsProps> = async ({ title, actionType, type }) => {
     <div className="bg-white" id="akcije">
       <div className="mx-auto max-w-2xl sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 flex flex-col items-center justify-center gap-20">
         <h1 className="text-3xl sm:text-4xl font-bold uppercase text-center text-green-600">
-          {title} {data.length}
+          {title}
         </h1>
         <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-8">
           {data
