3885b68623981a37f66bfecb300fd42092ea11df
nikolamirilo <nikolamirilo@gmail.com>
2023-06-21 00:33:53 +0200

    fix v5

--- app/api/actions/test/route.ts
+++ app/api/actions/test/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(request: NextRequest) {
   try {
    const random = Math.random();
-   return NextResponse.json(random, {headers:{"Cache-Control": "no-cache"}} )
+   return NextResponse.json(random, {headers:{"Cache-Control": "no-cache, no-store, max-age=0, must-revalidate"}} )
   } catch (error:any) {
     return new Response(error.message);
   }
