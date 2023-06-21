22011a843e1b46991c46c8b96935a98d326f2adf
Nikola <nikola.mirilo@adacta-fintech.com>
2023-06-21 13:00:42 +0200

    fixed ssr problem

--- app/api/actions/public/route.ts
+++ app/api/actions/public/route.ts
@@ -1,11 +1,13 @@
 import { getPublicActions } from "@/utils/actions";
 import { NextResponse } from "next/server";
 
+export const revalidate = 0;
+
 export async function GET() {
   try {
-    const publicActions  = await getPublicActions();
+    const publicActions = await getPublicActions();
     return NextResponse.json(publicActions);
-  } catch (error:any) {
+  } catch (error: any) {
     return new Response(error.message);
   }
 }
--- app/api/actions/route.ts
+++ app/api/actions/route.ts
@@ -1,12 +1,14 @@
 import { getActions } from "@/utils/actions";
 import { NextResponse } from "next/server";
 
+export const revalidate = 0;
+
 export async function GET() {
   try {
-    const allActions  = await getActions();
+    const allActions = await getActions();
     return NextResponse.json(allActions);
-  } catch (error:any) {
-    console.log(error)
+  } catch (error: any) {
+    console.log(error);
     return new Response(error.message);
   }
 }
