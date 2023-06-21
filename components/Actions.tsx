c382fe0fdeb5db6ff504c564a370f0d644bdaee0
Nikola <nikola.mirilo@adacta-fintech.com>
2023-06-21 13:12:28 +0200

    fixed problem with cards

--- components/Actions.tsx
+++ components/Actions.tsx
@@ -29,12 +29,12 @@ const Actions: React.FC<ActionsProps> = async ({ title, actionType, type }) => {
   }
   const data = await getData(actionType);
   return (
-    <div className="bg-white" id="akcije">
-      <div className="mx-auto max-w-2xl sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 flex flex-col items-center justify-center gap-20">
+    <div className="w-full" id="akcije">
+      <div className="mx-auto w-full max-w-2xl sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 flex flex-col items-center justify-center gap-20">
         <h1 className="text-3xl sm:text-4xl font-bold uppercase text-center text-green-600">
           {title}
         </h1>
-        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-8">
+        <div className="grid grid-cols-1 w-full gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-8">
           {data
             ? data.map((item: any, idx: number) => {
                 return (
--- components/Card.tsx
+++ components/Card.tsx
@@ -27,7 +27,7 @@ const Card: React.FC<CardProps> = ({
   }, []);
   return (
     <div className="block w-full rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
-      <div className="w-full h-56 hover:h-ful relative">
+      <div className="w-full h-64 hover:h-ful relative">
         <Image className="rounded-t-lg" src={image} alt={title} fill object-fit="cover" />
       </div>
       <div className="p-6">
