a0041c2b0db867ff8be7191c4646b5d0e237e25b
nikolamirilo <nikolamirilo@gmail.com>
2023-06-22 23:45:07 +0200

    fixed css style and added animations

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,16 +1,26 @@
-import Actions from "@/components/Actions";
 import AdminPanel from "@/components/AdminPanel";
+import Loader from "@/components/Loader";
 import Navbar from "@/components/Navbar";
 import dynamic from "next/dynamic";
 import React from "react";
 const Footer = dynamic(() => import("@/components/Footer"));
+const Actions = dynamic(() => import("@/components/Actions"), {
+  loading: () => <Loader />,
+});
 
 const Admin: React.FC = () => {
   return (
-    <div id="admin" className="flex flex-col justify-center items-center">
+    <div
+      id="admin"
+      className="flex flex-col justify-center items-center animate-fate"
+    >
       <Navbar />
       <AdminPanel>
-        <Actions title="Sve Akcije prijavljene od korisnika" actionType="actions" type="admin" />
+        <Actions
+          title="Sve Akcije prijavljene od korisnika"
+          actionType="actions"
+          type="admin"
+        />
       </AdminPanel>
       <Footer />
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
-import Actions from "@/components/Actions";
 import Hero from "@/components/Hero";
+import Loader from "@/components/Loader";
 import Navbar from "@/components/Navbar";
 import dynamic from "next/dynamic";
 import React from "react";
 const Partners = dynamic(() => import("@/components/Partners"), { ssr: false });
 const About = dynamic(() => import("@/components/About"));
 const Footer = dynamic(() => import("@/components/Footer"));
+const Actions = dynamic(() => import("@/components/Actions"), {
+  loading: () => <Loader />,
+});
 
 const Home: React.FC = async () => {
   return (
